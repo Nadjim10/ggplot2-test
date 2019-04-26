b46564377b75b0dab6c10c12d78612ade8931799
AdriClout <adrien.cloutier.1@ulaval.ca>
2019-04-25 20:12:40 -0400

    ajout c'est parti

---
+++ ggplot.R
@@ -0,0 +1,10 @@
+########################################## Mardi Scraping ##########################################
+
+# Bonjour! Voici le 2e document test d'Adrien Cloutier servant aux mardis scraping.
+# Dans ce document, je tenterai de faire un ggplot, en prévision du prochain meeting.
+
+# Bonne chance!
+
+########################################## 25 avril 2019 ##########################################
+
+# C'est parti! 
